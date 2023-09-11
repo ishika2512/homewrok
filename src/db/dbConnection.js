385ee9eb4a87786192098f55e97f04027235cdef
ishika2512 <eshikaparmar2001@gmail.com>
2023-09-11 10:26:44 +0530

    add file

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,13 +1,10 @@
-const mongoose = require("mongoose");
-const config = require("../config/config");
-const connectDB = async () => {
+const mongoose= require("mongoose");
+const config =  require ("../config/config");
+
+const connectDB=async () => {
     mongoose
-        .connect(config.mongodb.url, config.mongodb.options)
-        .then((data) => {
-            console.log('database connection successfullyyyyyyyyy!');
-        })
-        .catch((error) => {
-            console.log("database connection error: ", error);
-        });
-};
-module.exports = { connectDB };
+        .connect(config.mongodb.url, config.mongodb.opetions)
+        .then(data) => {
+            console.log("Database connection successfully..!");
+        }
+}
